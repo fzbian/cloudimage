c0cc2a1ea63a27993558483e4212c63a9abc9894
Fabian Martin <hello@fzbian.me>
2023-03-20 21:04:35 -0500

    feat: getting port from .env file

--- server.go
+++ server.go
@@ -3,17 +3,24 @@ package main
 import (
 	"cloudimage/controllers"
 	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"os"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
 	app := echo.New()
 
 	app.Static("/i/", "images")
 	app.GET("/health", controllers.Health)
 	app.POST("/upload", controllers.Upload)
 
-	err := app.Start(":8080")
+	err = app.Start(":" + os.Getenv("PORT"))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
